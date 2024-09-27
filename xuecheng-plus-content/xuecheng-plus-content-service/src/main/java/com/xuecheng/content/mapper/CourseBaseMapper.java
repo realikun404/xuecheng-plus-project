e3a22dd5cac178b3a99ba369d9bbd579a4354795
realikun404 <2253608218@qq.com>
2024-09-27 09:57:15 +0800

    编写测试文件，需要修改mybatis-plus-boot-starter.version为3.5.3

--- xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/mapper/CourseBaseMapper.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/mapper/CourseBaseMapper.java
@@ -2,6 +2,7 @@ package com.xuecheng.content.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.xuecheng.content.model.po.CourseBase;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * <p>
