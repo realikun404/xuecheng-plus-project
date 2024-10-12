1feba5efce5030342d5a30ae42fb294b8905ad04
realikun404 <2253608218@qq.com>
2024-10-12 14:31:27 +0800

    MinIO上传分片文件和合并分片测试代码编写

--- xuecheng-plus-media/xuecheng-plus-media-service/src/test/java/com/xuecheng/media/MinioTest.java
+++ xuecheng-plus-media/xuecheng-plus-media-service/src/test/java/com/xuecheng/media/MinioTest.java
@@ -2,19 +2,19 @@ package com.xuecheng.media;
 
 import com.j256.simplemagic.ContentInfo;
 import com.j256.simplemagic.ContentInfoUtil;
-import io.minio.GetObjectArgs;
-import io.minio.MinioClient;
-import io.minio.RemoveObjectArgs;
-import io.minio.UploadObjectArgs;
+import io.minio.*;
+import io.minio.errors.*;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.compress.utils.IOUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.FilterInputStream;
+import java.io.*;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class MinioTest {
 
@@ -82,6 +82,43 @@ public class MinioTest {
         if (source_md5.equals(local_md5)) {
             System.out.println("下载成功");
         }
+
+    }
+
+    //将分块文件上传到minio
+    @Test
+    public void uploadChunk() throws IOException, ServerException, InsufficientDataException, ErrorResponseException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
+
+        for (int i = 0; i < 10; i++) {
+            //上传文件的参数信息
+            UploadObjectArgs uploadObjectArgs = UploadObjectArgs.builder()
+                    .bucket("testbucket")//桶
+                    .filename("D:\\chunk\\"+i) //指定本地文件路径
+                    .object("chunk/"+i)//对象名 放在子目录下
+                    .build();
+
+            //上传文件
+            minioClient.uploadObject(uploadObjectArgs);
+            System.out.println("上传分块"+i+"成功");
+        }
+
+    }
+
+    //调用minio接口合并分块
+    @Test
+    public void testMerge() throws Exception {
+
+        List<ComposeSource> sources = Stream.iterate(0, i -> ++i).limit(10).map(i -> ComposeSource.builder().bucket("testbucket").object("chunk/" + i).build()).collect(Collectors.toList());
+
+        //指定合并后的objectName等信息
+        ComposeObjectArgs composeObjectArgs = ComposeObjectArgs.builder()
+                .bucket("testbucket")
+                .object("merge01.mp4")
+                .sources(sources)//指定源文件
+                .build();
+        //合并文件,
+        //报错size 1048576 must be greater than 5242880，minio默认的分块文件大小为5M
+        minioClient.composeObject(composeObjectArgs);
 
     }
 }
